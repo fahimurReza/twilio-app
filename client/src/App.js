86aca3d31800af122ba11146e40894b6bb9ac878
Auvy <fahimur@gmail.com>
2025-10-06 00:25:55 +0600

    Initial commit (without secrets)

---
+++ client/src/App.js
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react";
+import { Device } from "@twilio/voice-sdk";
+
+function App() {
+  const [device, setDevice] = useState(null);
+
+  useEffect(() => {
+    // Fetch token from backend
+    fetch("http://localhost:5000/token")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("Received Token:", data.token);
+
+        // Initialize Twilio Device with the token
+        const newDevice = new Device(data.token, { debug: true });
+
+        newDevice.on("ready", () => {
+          console.log("Twilio Device is ready to make/receive calls");
+        });
+
+        newDevice.on("error", (error) => {
+          console.error("Twilio Device Error:", error);
+        });
+
+        setDevice(newDevice);
+      })
+      .catch((err) => console.error(err));
+  }, []);
+
+  const makeCall = () => {
+    if (device) {
+      device.connect({ params: { To: "+13072077080" } });
+    } else {
+      console.log("Device not ready yet!");
+    }
+  };
+
+  return (
+    <div className="App">
+      <h1>Twilio Voice Client</h1>
+      <button onClick={makeCall} disabled={!device}>
+        Make a Call
+      </button>
+    </div>
+  );
+}
+
+export default App;
